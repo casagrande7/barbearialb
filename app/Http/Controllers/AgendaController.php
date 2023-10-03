8ace2674cebf55cb95b5ff9b48bd08f3ab8138ee
casagrande7 <joaofootz10@gmail.com>
2023-10-03 14:57:21 -0300

    Atualizado

--- app/Http/Controllers/AgendaController.php
+++ app/Http/Controllers/AgendaController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AgendaFormRequest;
+use App\Models\Agenda;
 use Illuminate\Http\Request;
 
 class AgendaController extends Controller
 {
-    
+    public function agenda(AgendaFormRequest $request){
+        $agenda= Agenda::create([
+        'profissional_id' => $request -> profissional,
+        'cliente_id' => $request -> cliente,
+        'servico_id' => $request -> servico,
+        'data_hora' => $request -> dataHora,
+        'tipo_pagamento' => $request -> tipoPagamento,
+        'valor' => $request -> valor
+
+        ]);
+        return response()->json([
+            "sucess" => true,
+            "message" => "Cliente cadastrado com sucesso",
+            "data" => $agenda
+        ], 200);
+    }
 }
